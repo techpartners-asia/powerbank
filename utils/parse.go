d183aef57bc21e374cf01f17a4f4ead215851a3f
Strong B Production <batbold.z@tehcpartners.asia>
2025-05-13 16:33:38 +0800

    Update ParseReturnPowerBankResponse and ParsePopupPowerBankResponse functions to adjust expected response lengths and reorganize response field assignments for improved clarity and correctness.

--- utils/parse.go
+++ utils/parse.go
@@ -10,8 +10,8 @@ import (
 )
 
 func ParseReturnPowerBankResponse(response []byte) (*powerbankModels.PowerBankReturnResponse, error) {
-	if len(response) < 9 {
-		return nil, fmt.Errorf("invalid data length: expected at least 9 bytes, got %d", len(response))
+	if len(response) < 15 {
+		return nil, fmt.Errorf("invalid data length: expected at least 15 bytes, got %d", len(response))
 	}
 
 	return &powerbankModels.PowerBankReturnResponse{
@@ -20,23 +20,17 @@ func ParseReturnPowerBankResponse(response []byte) (*powerbankModels.PowerBankRe
 		Cmd:          response[3],
 		ControlIndex: int(response[4]),
 		HoleIndex:    int(response[5]),
-		State:        int(response[6]),
-		Undefined1:   int(response[7]),
-		Undefined2:   int(response[8]),
-		Area:         string(response[9]),
-		PowerbankSN:  strconv.FormatUint(uint64(response[10])<<24|uint64(response[11])<<16|uint64(response[12])<<8|uint64(response[13]), 10),
-		SOC:          int(response[14]),
-		Temperature:  int(response[15]),
-		ChargeVolt:   float64(response[16]) / 10,
-		ChargeCurr:   float64(response[17]) / 10,
-		SoftVersion:  int(response[18]),
-		HardVersion:  int(response[19]),
-		Verify:       response[20],
+		Area:         string(response[6]),
+		PowerbankSN:  strconv.FormatUint(uint64(response[7])<<24|uint64(response[8])<<16|uint64(response[9])<<8|uint64(response[10]), 10),
+		State:        int(response[11]),
+		SoftVersion:  int(response[12]),
+		SOC:          int(response[13]),
+		Verify:       response[14],
 	}, nil
 }
 
 func ParsePopupPowerBankResponse(response []byte) (*powerbankModels.PowerBankPopupResponse, error) {
-	if len(response) < 21 {
+	if len(response) < 9 {
 		return nil, fmt.Errorf("invalid data length: expected at least 9 bytes, got %d", len(response))
 	}
 
@@ -46,9 +40,9 @@ func ParsePopupPowerBankResponse(response []byte) (*powerbankModels.PowerBankPop
 		Cmd:           response[3],
 		ControlIndex:  int(response[4]),
 		PowerbankSN:   strconv.FormatUint(uint64(response[5])<<24|uint64(response[6])<<16|uint64(response[7])<<8|uint64(response[8]), 10),
-		State:         int(response[6]),
-		SolenoidValve: int(response[7]),
-		Verify:        response[8],
+		State:         int(response[9]),
+		SolenoidValve: int(response[10]),
+		Verify:        response[11],
 	}, nil
 }
 
